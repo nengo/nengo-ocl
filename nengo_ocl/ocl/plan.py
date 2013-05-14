bb04cebedd96f1662e3cdc5aacad112e092d7807
James Bergstra <james.bergstra@gmail.com>
2013-05-14 11:29:12 -0400

    added Prog.enqueue_n_times and call_n_times

--- nengo_ocl/ocl/plan.py
+++ nengo_ocl/ocl/plan.py
@@ -24,6 +24,14 @@ class Prog(object):
         self.lsize = [p.lsize for p in self.plans]
 
     def __call__(self):
-        map(cl.enqueue_nd_range_kernel,
-            self.queues, self.kerns, self.gsize, self.lsize)
+        return self.call_n_times(1)
+
+    def call_n_times(self, n):
+        self.enqueue_n_times(n)
         self.queues[-1].finish()
+
+    def enqueue_n_times(self, n):
+        clrk = cl.enqueue_nd_range_kernel
+        qs, ks, gs, ls = self.queues, self.kerns, self.gsize, self.lsize
+        for ii in range(n):
+            map(clrk, qs, ks, gs, ls)
