667b9ee0270c1218dee6ac38bf04477cacd0b8aa
James Bergstra <james.bergstra@gmail.com>
2013-05-21 18:53:09 -0400

    moving stuff around

--- nengo_ocl/base.py
+++ nengo_ocl/base.py
@@ -1,151 +1,333 @@
-from ocl import array
-from ocl.plan import Prog
-from ocl.lif import plan_lif
-from ocl.gemv_batched import plan_misc_gemv
+import numpy as np
 
 
-class LIFMultiEnsemble(object):
+class DuplicateFilter(Exception):
+    pass
+
+
+def allsame(seq):
+    return len(set(seq)) < 2
+
+
+class Signal(object):
+    def __init__(self, n=1):
+        self.n = n
+
+
+class Population(object):
+    def __init__(self, n):
+        self.n = n
+
+
+class Transform(object):
+    def __init__(self, insig, outsig):
+        self.insig = insig
+        self.outsig = outsig
+
+
+class Filter(object):
+    def __init__(self, sig):
+        self.sig = sig
+
+
+class Encoder(object):
+    def __init__(self, sig, pop):
+        self.sig = sig
+        self.pop = pop
+        assert isinstance(sig, Signal)
+        assert isinstance(pop, Population)
+
+
+class Decoder(object):
+    def __init__(self, pop, sig):
+        self.pop = pop
+        self.sig = sig
+
+class Model(object):
+    def __init__(self):
+        self.signals = []
+        self.populations = []
+        self.encoders = []
+        self.decoders = []
+        self.transforms = []
+        self.filters = []
+
+    def signal(self, *args, **kwargs):
+        rval = Signal(*args, **kwargs)
+        self.signals.append(rval)
+        return rval
+
+    def population(self, *args, **kwargs):
+        rval = Population(*args, **kwargs)
+        self.populations.append(rval)
+        return rval
+
+    def encoder(self, sig, pop):
+        rval = Encoder(sig, pop)
+        self.encoders.append(rval)
+        return rval
+
+    def decoder(self, pop, sig):
+        rval = Decoder(pop, sig)
+        self.decoders.append(rval)
+        return rval
+
+    def transform(self, insig, outsig):
+        rval = Transform(insig, outsig)
+        self.transforms.append(rval)
+        return rval
+
+    def filter(self, insig, outsig):
+        rval = Filter(insig, outsig)
+        self.filters.append(rval)
+        return rval
+
+
+class RaggedArray(object):
+    # a linear buffer that is partitioned into
+    # sections of various lengths.
+    # 
+    def __init__(self, listoflists):
+
+        starts = []
+        lens = []
+        buf = []
+
+        for l in listoflists:
+            starts.append(len(buf))
+            lens.append(len(l))
+            buf.extend(l)
+
+        self.starts = starts
+        self.lens = lens
+        self.buf = np.asarray(buf)
+
+    def __len__(self):
+        return len(self.starts)
+
+    def __getitem__(self, item):
+        o = self.starts[item]
+        n = self.lens[item]
+        rval = self.buf[o: o + n]
+        if len(rval) != n:
+            raise ValueError('buf not long enough')
+        return rval
+
+    def __setitem__(self, item, val):
+        o = self.starts[item]
+        n = self.lens[item]
+        if len(val) != n:
+            raise ValueError('wrong len')
+        rval = self.buf[o: o + n]
+        if len(rval) != n:
+            raise ValueError('buf not long enough')
+        self.buf[o: o + n] = val
+
+
+def ragged_gather_gemv(Ms, Ns, alpha, A, A_js, X, X_js,
+                       beta, Y, Y_in=None):
     """
-    Object that allocates a bunch of buffers for
-    * lif neurons
-    * the signals they represent
-    * the encoders and decoders for relating the above
-
     """
-    def __init__(self, n_populations, n_neurons_per, n_signals, signal_size,
-            n_dec_per_signal=1,
-            n_enc_per_population=1,
-            lif_tau_rc=0.02,
-            lif_tau_ref=0.002,
-            lif_V_threshold=1.0,
-            lif_output_pstc=0.01,
-            lif_upsample=2,
-            noise=None,
-            queue=None):
-        self.__dict__.update(locals())
-        del self.self
+    try:
+        float(alpha)
+        alpha = [alpha] * len(Y)
+    except TypeError:
+        pass
 
-        self.lif_v = array.zeros(queue,
-                shape=(n_populations, n_neurons_per),
-                dtype='float32')
+    try:
+        float(beta)
+        beta = [beta] * len(Y)
+    except TypeError:
+        pass
 
-        self.lif_rt = array.zeros(queue,
-                shape=(n_populations, n_neurons_per),
-                dtype='float32')
+    if Y_in is None:
+        Y_in = Y
 
-        self.lif_ic = array.zeros(queue,
-                shape=(n_populations, n_neurons_per),
-                dtype='float32')
+    for i in xrange(len(Y)):
+        try:
+            y_i = beta[i] * Y_in[i]  # -- ragged getitem
+        except:
+            print i, beta, Y_in
+            raise
+        alpha_i = alpha[i]
 
-        self.lif_bias = array.zeros(queue,
-                shape=(n_populations, n_neurons_per),
-                dtype='float32')
+        x_js_i = X_js[i] # -- ragged getitem
+        A_js_i = A_js[i] # -- ragged getitem
+        
+        for xi, ai in zip(x_js_i, A_js_i):
+            x_ij = X[xi] # -- ragged getitem
+            M_i = Ms[ai]
+            N_i = Ns[ai]
+            assert N_i == len(x_ij)
+            A_ij = A[ai].reshape(N_i, M_i) # -- ragged getitem
+            try:
+                y_i += alpha_i * np.dot(x_ij, A_ij.reshape(N_i, M_i))
+            except:
+                print i, xi, ai, A_ij, x_ij
+                raise
 
-        self.lif_output = array.zeros(queue,
-                shape=(n_populations, n_neurons_per),
-                dtype='float32')
+        Y[i] = y_i
 
-        self.lif_output_filter = array.zeros(queue,
-                shape=(n_populations, n_neurons_per),
-                dtype='float32')
 
-        self.signals = array.zeros(queue,
-                shape=(n_signals, signal_size),
-                dtype='float32')
+class Simulator(object):
+    def __init__(self, model):
+        self.model = model
+        self.sidx = dict((s, i) for (i, s) in enumerate(model.signals))
+        self.pidx = dict((p, i) for (i, p) in enumerate(model.populations))
+        self.eidx = dict((s, i) for (i, s) in enumerate(model.encoders))
+        self.didx = dict((p, i) for (i, p) in enumerate(model.decoders))
 
-        self.encoders = array.zeros(queue,
-                shape=(n_populations, n_neurons_per, n_enc_per_population,
-                    signal_size),
-                dtype='float32')
-
-        self.encoders_signal_idx = array.zeros(queue,
-                shape=(n_populations, n_enc_per_population),
-                dtype='int32')
-
-        self.decoders = array.zeros(queue,
-                shape=(n_signals, signal_size, n_dec_per_signal,
-                    n_neurons_per),
-                dtype='float32')
-
-        self.decoders_population_idx = array.zeros(queue,
-                shape=(n_signals, n_dec_per_signal),
-                dtype='int32')
-
-    def realloc_for_GPU(self):
-        self.realloc_encoders_for_GPU()
-        self.realloc_decoders_for_GPU()
-
-    def realloc_encoders_for_GPU(self):
-        encoders = self.encoders.get(self.queue)
-        encoders = encoders.transpose(3, 2, 0, 1).copy()
-        cmajor_cl_encoders = array.to_device(self.queue, encoders)
-        self.encoders = cmajor_cl_encoders.transpose(2, 3, 1, 0)
-
-    def realloc_decoders_for_GPU(self):
-        decoders = self.decoders.get(self.queue)
-        decoders = decoders.transpose(3, 2, 0, 1).copy()
-        cmajor_cl_decoders = array.to_device(self.queue, decoders)
-        self.decoders = cmajor_cl_decoders.transpose(2, 3, 1, 0)
-
-    def neuron_plan(self, dt):
-        # XXX add support for built-in filtering
-        rval = plan_lif(self.queue,
-                V=self.lif_v,
-                RT=self.lif_rt,
-                J=self.lif_ic,
-                OV=self.lif_v,
-                ORT=self.lif_rt,
-                OS=self.lif_output,
-                OSfilt=self.lif_output_filter,
-                dt=dt,
-                tau_rc=self.lif_tau_rc,
-                tau_ref=self.lif_tau_ref,
-                V_threshold=self.lif_V_threshold,
-                upsample=self.lif_upsample,
-                pstc=self.lif_output_pstc,
-                )
-        return rval
-
-    def decoder_plan(self, signals_beta):
-        # XXX: do not overwrite the entire signals vector
-        #       when decoding, some of the signals are not
-        #       decoded from these populations.
-        if self.n_dec_per_signal != 1:
+        if not all(s.n == 1 for s in self.model.signals):
             raise NotImplementedError()
-        rval = plan_misc_gemv(self.queue,
-                alpha=1.0,
-                A=self.decoders[:, :, 0],
-                X=self.lif_output_filter,
-                Xi=self.decoders_population_idx[:, 0],
-                beta=signals_beta,
-                Y=self.signals,
-                tag='decoders')
-        return rval
 
-    def encoder_plan(self):
-        if self.n_enc_per_population != 1:
-            raise NotImplementedError()
-        rval = plan_misc_gemv(self.queue,
-                alpha=1.0,
-                A=self.encoders[:, :, 0],
-                X=self.signals,
-                Xi=self.encoders_signal_idx[:, 0],
-                beta=1.0,
-                Y_in=self.lif_bias,
-                Y=self.lif_ic,
-                tag='encoders')
-        return rval
+    def alloc_signals(self):
+        self.sigs = RaggedArray([[0] for s in self.model.signals])
+        
+        # filtered, transformed signals
+        self.tf_sigs = RaggedArray([[0] for s in self.model.signals])
 
-    def prog(self, dt):
-        """
-        A single loop through the simulator does the following things in order:
+    def alloc_populations(self):
+        def pop_props():
+            return RaggedArray(
+                [[0] * p.n for p in self.model.populations])
+        # -- lif-specific stuff
+        self.pop_ic = pop_props()
+        self.pop_jbias = pop_props()
+        self.pop_voltage = pop_props()
+        self.pop_rt = pop_props()
+        self.pop_output = pop_props()
 
-        """
-        return Prog([
-            self.encoder_plan(),
-            self.neuron_plan(dt=dt),
-            self.decoder_plan(signals_beta=0.0),
-        ])
+    def alloc_filters_and_transforms(self):
+        transforms = self.model.transforms
+        filters = self.model.filters
+        signals = self.model.signals
+
+        filtersig = {}
+        for filt in filters:
+            if filt.sig in filtersig:
+                raise DuplicateFilter('signal has multiple filters',
+                                     filt.sig)
+            # XXX use exp(), dt etc. to calculate filter coef
+            filtersig[filt.sig] = .01
+
+        self.tf_alpha = np.asarray([1 - filtersig.get(sig, 0)
+                                    for sig in signals])
+        self.tf_beta = np.asarray([filtersig.get(sig, 0)
+                                   for sig in signals])
+
+        self.tf_weights = RaggedArray([[0.1] for tf in transforms])
+        self.tf_Ns = [1] * len(transforms)
+        self.tf_Ms = [1] * len(transforms)
+
+        # -- which transform(s) does each signal use
+        tidx = dict((tf, i) for (i, tf) in enumerate(transforms))
+        self.tf_weights_js = RaggedArray([
+            [tidx[tf] for tf in transforms if tf.outsig == sig]
+            for sig in self.model.signals
+            ])
+
+        # -- which corresponding(s) signal is transformed
+        self.tf_signals_js = RaggedArray([
+            [self.sidx[tf.insig] for tf in transforms if tf.outsig == sig]
+            for sig in self.model.signals
+            ])
+
+
+    def alloc_encoders(self):
+        encoders = self.model.encoders
+        self.enc_weights = RaggedArray(
+            [np.random.randn(enc.pop.n) for enc in encoders])
+
+        self.enc_Ms = [enc.pop.n for enc in encoders]
+        self.enc_Ns = [1] * len(encoders)
+
+        # -- which encoder(s) does each population use
+        self.enc_weights_js = [
+            [self.eidx[enc] for enc in encoders if enc.pop == pop]
+            for pop in self.model.populations]
+
+        # -- and which corresponding signal does it encode
+        self.enc_signals_js = [
+            [self.sidx[enc.sig]
+                for enc in encoders if enc.pop == pop]
+            for pop in self.model.populations]
+
+    def alloc_decoders(self):
+        decoders = self.model.decoders
+        self.dec_weights = RaggedArray(
+            [np.random.randn(dec.pop.n) for dec in decoders])
+        self.dec_Ms = [1] * len(decoders)
+        self.dec_Ns = [dec.pop.n for dec in decoders]
+
+        # -- which decoder(s) does each signal use
+        self.dec_weights_js = [
+            [self.didx[dec] for dec in decoders if dec.sig == sig]
+            for sig in self.model.signals]
+
+        # -- and which corresponding population does it decode
+        self.dec_pops_js = [
+            [self.pidx[dec.pop]
+                for dec in decoders if dec.sig == sig]
+            for sig in self.model.signals]
+
+    def alloc_all(self):
+        self.alloc_signals()
+        self.alloc_populations()
+        self.alloc_filters_and_transforms()
+        self.alloc_encoders()
+        self.alloc_decoders()
+
+
+    def do_signal_transform(self):
+        ragged_gather_gemv(
+            Ms=self.tf_Ms,
+            Ns=self.tf_Ns,
+            alpha=self.tf_alpha,
+            A=self.tf_weights,
+            A_js=self.tf_weights_js,
+            X=self.sigs,
+            X_js=self.tf_signals_js,
+            beta=self.tf_beta,
+            Y=self.tf_sigs,
+            )
+
+    def do_encoders(self):
+        ragged_gather_gemv(
+            Ms=self.enc_Ms,
+            Ns=self.enc_Ns,
+            alpha=1.0,
+            A=self.enc_weights,
+            A_js=self.enc_weights_js,
+            X=self.tf_sigs,
+            X_js=self.enc_signals_js,
+            beta=1.0,
+            Y_in=self.pop_jbias,
+            Y=self.pop_ic,
+            )
+
+    def do_populations(self):
+        pass
+
+    def do_decoders(self):
+        ragged_gather_gemv(
+            Ms=self.dec_Ms,
+            Ns=self.dec_Ns,
+            alpha=1.0,
+            A=self.dec_weights,
+            A_js=self.dec_weights_js,
+            X=self.pop_output,
+            X_js=self.dec_pops_js,
+            beta=0.0,
+            Y=self.sigs,
+            )
+
+
+    def step(self):
+        self.do_signal_transform()
+        self.do_encoders()
+        self.do_populations()
+        self.do_decoders()
+
 
 
 
--- nengo_ocl/test_base.py
+++ nengo_ocl/test_base.py
@@ -6,8 +6,155 @@ import pyopencl as cl
 
 ctx = cl.create_some_context()
 
-from base import LIFMultiEnsemble
 from nengo import nef_theano as nef
+
+
+class LIFMultiEnsemble(object):
+    """
+    Object that allocates a bunch of buffers for
+    * lif neurons
+    * the signals they represent
+    * the encoders and decoders for relating the above
+
+    """
+    def __init__(self, n_populations, n_neurons_per, n_signals, signal_size,
+            n_dec_per_signal=1,
+            n_enc_per_population=1,
+            lif_tau_rc=0.02,
+            lif_tau_ref=0.002,
+            lif_V_threshold=1.0,
+            lif_output_pstc=0.01,
+            lif_upsample=2,
+            noise=None,
+            queue=None):
+        self.__dict__.update(locals())
+        del self.self
+
+        self.lif_v = array.zeros(queue,
+                shape=(n_populations, n_neurons_per),
+                dtype='float32')
+
+        self.lif_rt = array.zeros(queue,
+                shape=(n_populations, n_neurons_per),
+                dtype='float32')
+
+        self.lif_ic = array.zeros(queue,
+                shape=(n_populations, n_neurons_per),
+                dtype='float32')
+
+        self.lif_bias = array.zeros(queue,
+                shape=(n_populations, n_neurons_per),
+                dtype='float32')
+
+        self.lif_output = array.zeros(queue,
+                shape=(n_populations, n_neurons_per),
+                dtype='float32')
+
+        self.lif_output_filter = array.zeros(queue,
+                shape=(n_populations, n_neurons_per),
+                dtype='float32')
+
+        self.signals = array.zeros(queue,
+                shape=(n_signals, signal_size),
+                dtype='float32')
+
+        self.encoders = array.zeros(queue,
+                shape=(n_populations, n_neurons_per, n_enc_per_population,
+                    signal_size),
+                dtype='float32')
+
+        self.encoders_signal_idx = array.zeros(queue,
+                shape=(n_populations, n_enc_per_population),
+                dtype='int32')
+
+        self.decoders = array.zeros(queue,
+                shape=(n_signals, signal_size, n_dec_per_signal,
+                    n_neurons_per),
+                dtype='float32')
+
+        self.decoders_population_idx = array.zeros(queue,
+                shape=(n_signals, n_dec_per_signal),
+                dtype='int32')
+
+    def realloc_for_GPU(self):
+        self.realloc_encoders_for_GPU()
+        self.realloc_decoders_for_GPU()
+
+    def realloc_encoders_for_GPU(self):
+        encoders = self.encoders.get(self.queue)
+        encoders = encoders.transpose(3, 2, 0, 1).copy()
+        cmajor_cl_encoders = array.to_device(self.queue, encoders)
+        self.encoders = cmajor_cl_encoders.transpose(2, 3, 1, 0)
+
+    def realloc_decoders_for_GPU(self):
+        decoders = self.decoders.get(self.queue)
+        decoders = decoders.transpose(3, 2, 0, 1).copy()
+        cmajor_cl_decoders = array.to_device(self.queue, decoders)
+        self.decoders = cmajor_cl_decoders.transpose(2, 3, 1, 0)
+
+    def neuron_plan(self, dt):
+        # XXX add support for built-in filtering
+        rval = plan_lif(self.queue,
+                V=self.lif_v,
+                RT=self.lif_rt,
+                J=self.lif_ic,
+                OV=self.lif_v,
+                ORT=self.lif_rt,
+                OS=self.lif_output,
+                OSfilt=self.lif_output_filter,
+                dt=dt,
+                tau_rc=self.lif_tau_rc,
+                tau_ref=self.lif_tau_ref,
+                V_threshold=self.lif_V_threshold,
+                upsample=self.lif_upsample,
+                pstc=self.lif_output_pstc,
+                )
+        return rval
+
+    def decoder_plan(self, signals_beta):
+        # XXX: do not overwrite the entire signals vector
+        #       when decoding, some of the signals are not
+        #       decoded from these populations.
+        if self.n_dec_per_signal != 1:
+            raise NotImplementedError()
+        rval = plan_misc_gemv(self.queue,
+                alpha=1.0,
+                A=self.decoders[:, :, 0],
+                X=self.lif_output_filter,
+                Xi=self.decoders_population_idx[:, 0],
+                beta=signals_beta,
+                Y=self.signals,
+                tag='decoders')
+        return rval
+
+    def encoder_plan(self):
+        if self.n_enc_per_population != 1:
+            raise NotImplementedError()
+        rval = plan_misc_gemv(self.queue,
+                alpha=1.0,
+                A=self.encoders[:, :, 0],
+                X=self.signals,
+                Xi=self.encoders_signal_idx[:, 0],
+                beta=1.0,
+                Y_in=self.lif_bias,
+                Y=self.lif_ic,
+                tag='encoders')
+        return rval
+
+    def prog(self, dt):
+        """
+        A single loop through the simulator does the following things in order:
+
+        """
+        return Prog([
+            self.encoder_plan(),
+            self.neuron_plan(dt=dt),
+            self.decoder_plan(signals_beta=0.0),
+        ])
+
+
+
+
 
 def test_one_random():
     queue = cl.CommandQueue(ctx,
