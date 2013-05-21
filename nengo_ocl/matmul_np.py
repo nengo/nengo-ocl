f06fdbcc37ed9a3ecc760bc425ddecd2ca53bcbe
James Bergstra <james.bergstra@gmail.com>
2013-05-21 18:50:31 -0400

    matmul base

--- nengo_ocl/matmul_np.py
+++ nengo_ocl/matmul_np.py
@@ -1,13 +1,17 @@
+
 import numpy as np
+
+class DuplicateFilter(Exception):
+    pass
+
 
 def allsame(seq):
     return len(set(seq)) < 2
 
 
 class Signal(object):
-    def __init__(self, n=1, pstc=0):
+    def __init__(self, n=1):
         self.n = n
-        self.pstc = pstc
 
 
 class Population(object):
@@ -16,44 +20,28 @@ class Population(object):
 
 
 class Transform(object):
-    def __init__(self, insigs, outsig):
-        self.insigs = insigs
+    def __init__(self, insig, outsig):
+        self.insig = insig
         self.outsig = outsig
+
+
+class Filter(object):
+    def __init__(self, sig):
+        self.sig = sig
 
 
 class Encoder(object):
     def __init__(self, sig, pop):
         self.sig = sig
         self.pop = pop
+        assert isinstance(sig, Signal)
+        assert isinstance(pop, Population)
 
 
 class Decoder(object):
     def __init__(self, pop, sig):
         self.pop = pop
         self.sig = sig
-
-
-class RaggedArrayD(object):
-    # a linear buffer that is partitioned into
-    # sections of various lengths.
-    # 
-    def __init__(self, keys, lens, buf=None):
-        chunks = {}
-        offset = 0
-        for k, l, in zip(keys, lens):
-            chunks[k] = (offset, l)
-            offset += l
-        self.chunks = chunks
-        self.keys = keys
-        if buf is None:
-            self.buf = np.zeros(offset)
-        else:
-            self.buf = buf
-
-        self.idx = dict((k, i) for i, k in enumerate(keys))
-        self.offsets = dict((k, chunks[k][0]) for k in keys)
-        self.lens = dict((k, chunks[k][1]) for k in keys)
-
 
 class Model(object):
     def __init__(self):
@@ -62,6 +50,7 @@ class Model(object):
         self.encoders = []
         self.decoders = []
         self.transforms = []
+        self.filters = []
 
     def signal(self, *args, **kwargs):
         rval = Signal(*args, **kwargs)
@@ -83,78 +72,262 @@ class Model(object):
         self.decoders.append(rval)
         return rval
 
-    def transform(self, insigs, outsig):
-        rval = Transform(insigs, outsig)
+    def transform(self, insig, outsig):
+        rval = Transform(insig, outsig)
         self.transforms.append(rval)
         return rval
+
+    def filter(self, insig, outsig):
+        rval = Filter(insig, outsig)
+        self.filters.append(rval)
+        return rval
+
+
+
+class RaggedArray(object):
+    # a linear buffer that is partitioned into
+    # sections of various lengths.
+    # 
+    def __init__(self, listoflists):
+
+        starts = []
+        lens = []
+        buf = []
+
+        for l in listoflists:
+            starts.append(len(buf))
+            lens.append(len(l))
+            buf.extend(l)
+
+        self.starts = starts
+        self.lens = lens
+        self.buf = np.asarray(buf)
+
+    def __len__(self):
+        return len(self.starts)
+
+    def __getitem__(self, item):
+        o = self.starts[item]
+        n = self.lens[item]
+        rval = self.buf[o: o + n]
+        if len(rval) != n:
+            raise ValueError('buf not long enough')
+        return rval
+
+    def __setitem__(self, item, val):
+        o = self.starts[item]
+        n = self.lens[item]
+        if len(val) != n:
+            raise ValueError('wrong len')
+        rval = self.buf[o: o + n]
+        if len(rval) != n:
+            raise ValueError('buf not long enough')
+        self.buf[o: o + n] = val
+
+
+def ragged_gather_gemv(Ms, Ns, alpha, A, A_js, X, X_js,
+                       beta, Y, Y_in=None):
+    """
+    """
+    try:
+        float(alpha)
+        alpha = [alpha] * len(Y)
+    except TypeError:
+        pass
+
+    try:
+        float(beta)
+        beta = [beta] * len(Y)
+    except TypeError:
+        pass
+
+    if Y_in is None:
+        Y_in = Y
+
+    for i in xrange(len(Y)):
+        try:
+            y_i = beta[i] * Y_in[i]  # -- ragged getitem
+        except:
+            print i, beta, Y_in
+            raise
+        alpha_i = alpha[i]
+
+        x_js_i = X_js[i] # -- ragged getitem
+        A_js_i = A_js[i] # -- ragged getitem
+        
+        for xi, ai in zip(x_js_i, A_js_i):
+            x_ij = X[xi] # -- ragged getitem
+            M_i = Ms[ai]
+            N_i = Ns[ai]
+            assert N_i == len(x_ij)
+            A_ij = A[ai].reshape(N_i, M_i) # -- ragged getitem
+            try:
+                y_i += alpha_i * np.dot(x_ij, A_ij.reshape(N_i, M_i))
+            except:
+                print i, xi, ai, A_ij, x_ij
+                raise
+
+        Y[i] = y_i
 
 
 class Simulator(object):
     def __init__(self, model):
         self.model = model
+        self.sidx = dict((s, i) for (i, s) in enumerate(model.signals))
+        self.pidx = dict((p, i) for (i, p) in enumerate(model.populations))
+        self.eidx = dict((s, i) for (i, s) in enumerate(model.encoders))
+        self.didx = dict((p, i) for (i, p) in enumerate(model.decoders))
+
+        if not all(s.n == 1 for s in self.model.signals):
+            raise NotImplementedError()
 
     def alloc_signals(self):
-        self.asigs = RaggedArrayD(
-            self.model.signals,
-            [1] * len(self.model.signals))
+        self.sigs = RaggedArray([[0] for s in self.model.signals])
+        
+        # filtered, transformed signals
+        self.tf_sigs = RaggedArray([[0] for s in self.model.signals])
 
     def alloc_populations(self):
-        self.apops = RaggedArrayD(
-            self.model.populations,
-            [p.n for p in self.model.populations])
+        def pop_props():
+            return RaggedArray(
+                [[0] * p.n for p in self.model.populations])
+        # -- lif-specific stuff
+        self.pop_ic = pop_props()
+        self.pop_jbias = pop_props()
+        self.pop_voltage = pop_props()
+        self.pop_rt = pop_props()
+        self.pop_output = pop_props()
 
-    def alloc_transforms(self):
-        asigs = self.asigs
+    def alloc_filters_and_transforms(self):
+        transforms = self.model.transforms
+        filters = self.model.filters
+        signals = self.model.signals
 
-        isigs = [[asigs.offsets[k]] for k in asigs.keys]
-        for tf in self.model.transforms:
-            isigs[asigs.idx[tf.outsig]].extend(
-                [asigs.idx[insig] for insig in tf.insigs])
+        filtersig = {}
+        for filt in filters:
+            if filt.sig in filtersig:
+                raise DuplicateFilter('signal has multiple filters',
+                                     filt.sig)
+            # XXX use exp(), dt etc. to calculate filter coef
+            filtersig[filt.sig] = .01
 
-        def flatten(ll):
-            rval = []
-            for l in ll:
-                rval.extend(l)
-            return rval
+        self.tf_alpha = np.asarray([1 - filtersig.get(sig, 0)
+                                    for sig in signals])
+        self.tf_beta = np.asarray([filtersig.get(sig, 0)
+                                   for sig in signals])
 
-        tf_ipos = RaggedArrayD(asigs.keys, map(len, isigs))
-        tf_ipos.buf = np.asarray(flatten(isigs), dtype='int')
+        self.tf_weights = RaggedArray([[0.1] for tf in transforms])
+        self.tf_Ns = [1] * len(transforms)
+        self.tf_Ms = [1] * len(transforms)
 
-        tf_weights = RaggedArrayD(asigs.keys, map(len, isigs))
-        # XXX
-        # the  weights should be the pstc corrected for dt
-        # in the case of the self-connections  (filters)
-        # and it should be the multipliers on other signals
-        # in the case of general transforms
-        tf_weights.buf += .1  # -- this should be pstc corrected for dt
+        # -- which transform(s) does each signal use
+        tidx = dict((tf, i) for (i, tf) in enumerate(transforms))
+        self.tf_weights_js = RaggedArray([
+            [tidx[tf] for tf in transforms if tf.outsig == sig]
+            for sig in self.model.signals
+            ])
 
-        # XXX
-        # this is the other part of the filter, which should
-        # also be corrected for dt.
-        tf_beta = np.zeros(len(isigs)) + .9
+        # -- which corresponding(s) signal is transformed
+        self.tf_signals_js = RaggedArray([
+            [self.sidx[tf.insig] for tf in transforms if tf.outsig == sig]
+            for sig in self.model.signals
+            ])
 
-        self.tf_ipos = tf_ipos
-        self.tf_weights = tf_weights
-        self.tf_beta = tf_beta
 
     def alloc_encoders(self):
-        self.enc_weights = RaggedArrayD(
-            self.model.encoders,
-            [enc.pop.n for enc in self.model.encoders])
+        encoders = self.model.encoders
+        self.enc_weights = RaggedArray(
+            [np.random.randn(enc.pop.n) for enc in encoders])
+
+        self.enc_Ms = [enc.pop.n for enc in encoders]
+        self.enc_Ns = [1] * len(encoders)
+
+        # -- which encoder(s) does each population use
+        self.enc_weights_js = [
+            [self.eidx[enc] for enc in encoders if enc.pop == pop]
+            for pop in self.model.populations]
+
+        # -- and which corresponding signal does it encode
+        self.enc_signals_js = [
+            [self.sidx[enc.sig]
+                for enc in encoders if enc.pop == pop]
+            for pop in self.model.populations]
 
     def alloc_decoders(self):
-        self.dec_weights = RaggedArrayD(
-            self.model.decoders,
-            [dec.pop.n for dec in self.model.encoders])
+        decoders = self.model.decoders
+        self.dec_weights = RaggedArray(
+            [np.random.randn(dec.pop.n) for dec in decoders])
+        self.dec_Ms = [1] * len(decoders)
+        self.dec_Ns = [dec.pop.n for dec in decoders]
+
+        # -- which decoder(s) does each signal use
+        self.dec_weights_js = [
+            [self.didx[dec] for dec in decoders if dec.sig == sig]
+            for sig in self.model.signals]
+
+        # -- and which corresponding population does it decode
+        self.dec_pops_js = [
+            [self.pidx[dec.pop]
+                for dec in decoders if dec.sig == sig]
+            for sig in self.model.signals]
 
     def alloc_all(self):
         self.alloc_signals()
         self.alloc_populations()
-        self.alloc_transforms()
+        self.alloc_filters_and_transforms()
         self.alloc_encoders()
         self.alloc_decoders()
 
 
+    def do_signal_transform(self):
+        ragged_gather_gemv(
+            Ms=self.tf_Ms,
+            Ns=self.tf_Ns,
+            alpha=self.tf_alpha,
+            A=self.tf_weights,
+            A_js=self.tf_weights_js,
+            X=self.sigs,
+            X_js=self.tf_signals_js,
+            beta=self.tf_beta,
+            Y=self.tf_sigs,
+            )
+
+    def do_encoders(self):
+        ragged_gather_gemv(
+            Ms=self.enc_Ms,
+            Ns=self.enc_Ns,
+            alpha=1.0,
+            A=self.enc_weights,
+            A_js=self.enc_weights_js,
+            X=self.tf_sigs,
+            X_js=self.enc_signals_js,
+            beta=1.0,
+            Y_in=self.pop_jbias,
+            Y=self.pop_ic,
+            )
+
+    def do_populations(self):
+        pass
+
+    def do_decoders(self):
+        ragged_gather_gemv(
+            Ms=self.dec_Ms,
+            Ns=self.dec_Ns,
+            alpha=1.0,
+            A=self.dec_weights,
+            A_js=self.dec_weights_js,
+            X=self.pop_output,
+            X_js=self.dec_pops_js,
+            beta=0.0,
+            Y=self.sigs,
+            )
+
+
+    def step(self):
+        self.do_signal_transform()
+        self.do_encoders()
+        self.do_populations()
+        self.do_decoders()
 
 
 
@@ -209,9 +382,8 @@ def test_matrix_mult_example():
                 C[(i, j, k)] = m.population(200)
                 C_in[(i, j, k)] = m.signal()
                 C_dec[(i, j, k)] = m.signal()
-                m.transform(
-                    [A_dec[(i, j)], B_dec[(j, k)]],
-                    C_in[(i, j, k)])
+                m.transform(A_dec[(i, j)], C_in[(i, j, k)])
+                m.transform(B_dec[(i, j)], C_in[(i, j, k)])
                 m.encoder(C_in[(i, j, k)], C[(i, j, k)])
                 m.decoder(C[(i, j, k)], C_dec[(i, j, k)])
 
@@ -223,12 +395,12 @@ def test_matrix_mult_example():
             D[(i, j)] = m.population(50)
             D_in[(i, j)] = m.signal()
             D_dec[(i, j)] = m.signal()
-            m.transform(
-                [C_dec[(i, k, j)] for k in range(D2)],
-                D_in[(i, j)])
-            m.encoder([D_in[(i, j)]], D[(i, j)])
+            for k in range(D2):
+                m.transform(C_dec[(i, k, j)], D_in[(i, j)])
+            m.encoder(D_in[(i, j)], D[(i, j)])
             m.decoder(D[(i, j)], D_dec[(i, j)])
 
     sim = Simulator(m)
 
     sim.alloc_all()
+    sim.step()
