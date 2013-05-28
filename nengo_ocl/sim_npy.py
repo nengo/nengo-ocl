6617017dab2c2f19ccee56b78cab3441633f7e8b
James Bergstra <james.bergstra@gmail.com>
2013-05-28 18:37:11 -0400

    moved lif_step to simulator.py

--- nengo_ocl/sim_npy.py
+++ nengo_ocl/sim_npy.py
@@ -4,6 +4,8 @@ numpy Simulator in the style of the OpenCL one, to get design right.
 import math
 import numpy as np
 
+from simulator import lif_step
+
 
 class RaggedArray(object):
     # a linear buffer that is partitioned into
@@ -160,44 +162,6 @@ def ragged_gather_gemv(Ms, Ns, alpha, A, A_js, X, X_js,
             Y[i] = y_i
 
 
-def lif_step(J, voltage, refractory_time, spiked, dt, tau_rc, tau_ref, upsample):
-    if upsample != 1.0:
-        raise NotImplementedError()
-
-    # Euler's method
-    dV = dt / tau_rc * (J - voltage)
-
-    # increase the voltage, ignore values below 0
-    v = np.maximum(voltage + dV, 0)  
-    
-    # handle refractory period        
-    post_ref = 1.0 - (refractory_time - dt) / dt
-
-    # set any post_ref elements < 0 = 0, and > 1 = 1
-    v *= np.clip(post_ref, 0, 1)
-    
-    # determine which neurons spike
-    # if v > 1 set spiked = 1, else 0
-    spiked[:] = (v > 1) * 1.0
-    
-    # adjust refractory time (neurons that spike get
-    # a new refractory time set, all others get it reduced by dt)
-
-    # linearly approximate time since neuron crossed spike threshold
-    overshoot = (v - 1) / dV 
-    spiketime = dt * (1.0 - overshoot)
-
-    # adjust refractory time (neurons that spike get a new
-    # refractory time set, all others get it reduced by dt)
-    new_refractory_time = spiked * (spiketime + tau_ref) \
-            + (1 - spiked) * (refractory_time - dt)
-
-    # return an ordered dictionary of internal variables to update
-    # (including setting a neuron that spikes to a voltage of 0)
-
-    voltage[:] = v * (1 - spiked)
-    refractory_time[:] = new_refractory_time
-    
 
 class Simulator(object):
     def __init__(self, model, n_prealloc_probes=1000):
