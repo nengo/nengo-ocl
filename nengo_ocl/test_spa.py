03ff06a1f1837564bda1678730d14bb1f5d597c4
James Bergstra <james.bergstra@gmail.com>
2013-05-31 00:15:58 -0400

    cleaner circular convolution test

--- nengo_ocl/test_spa.py
+++ nengo_ocl/test_spa.py
@@ -6,68 +6,60 @@ from sim_ocl import Simulator
 
 ctx = cl.create_some_context()
 
-D = 500  # dimensionality of SPA
-
-neurons_per_input = 64
-neurons_per_product = 128 # -- way faster than 150
+D = 4  # dimensionality of SPA
 
 m = Model(.001)
 
-dummy = np.random.randn(D, D)
-
 A = [m.signal() for ii in range(D)]
 B = [m.signal() for ii in range(D)]
-AF = [m.signal() for ii in range(D)]
-BF = [m.signal() for ii in range(D)]
-CF = [m.signal() for ii in range(D)]
-C = [m.signal() for ii in range(D)]
-
 
 for ii in range(D):
     m.filter(1.0, A[ii], A[ii])
     m.filter(1.0, B[ii], B[ii])
 
-for ii in range(D):
-    for jj in range(D):
-        m.filter(dummy[ii, jj], A[ii], AF[jj])
-        m.filter(dummy[ii, jj], B[ii], BF[jj])
 
-for jj in range(4):
-    for ii in range(D):
-        pop = m.population(neurons_per_product)
-        m.encoder(AF[ii], pop)
-        m.encoder(BF[ii], pop)
-        m.decoder(pop, CF[ii])
+class CircularConvolution(object):
+    # XXX actually create a Fourier matrix here
+    fourier_matrix = np.random.randn(D, D)
 
-if 0:
-    # second circular convolution
-    for jj in range(4):
+    def __init__(self, model, A, B,
+            neurons_per_product=128,
+            neuron_type=None):
+        D = len(A)
+        # -- the output signals
+        self.C = [m.signal() for ii in range(D)]
+
+        # -- Fourier transforms of A, B, C
+        self.AF = [m.signal() for ii in range(D)]
+        self.BF = [m.signal() for ii in range(D)]
+        self.CF = [m.signal() for ii in range(D)]
+
+        # -- compute the Fourier transform of A and B
+        #    as Filters, so populations are delayed by
+        #    one time step.
         for ii in range(D):
-            pop = m.population(neurons_per_product)
-            m.encoder(AF[ii], pop)
-            m.encoder(BF[ii], pop)
-            m.decoder(pop, C[ii])
+            for jj in range(D):
+                alpha = self.fourier_matrix[ii, jj]
+                m.filter(alpha, A[ii], self.AF[jj])
+                m.filter(alpha, B[ii], self.BF[jj])
 
-    # third circular convolution
-    for jj in range(4):
+        # -- compute the complex elementwise product of A, B
+        #    in Fourier domain
+        for jj in range(4):
+            for ii in range(D):
+                pop = m.population(neurons_per_product)
+                m.encoder(self.AF[ii], pop)
+                m.encoder(self.BF[ii], pop)
+                m.decoder(pop, self.CF[ii])
+
+        # -- compute inverse Fourier transform of decoded
+        #    products into output signal C
         for ii in range(D):
-            pop = m.population(neurons_per_product)
-            m.encoder(AF[ii], pop)
-            m.encoder(BF[ii], pop)
-            m.decoder(pop, A[ii])
+            for jj in range(D):
+                alpha = self.fourier_matrix[ii, jj]
+                m.transform(alpha, self.CF[jj], self.C[ii])
 
-    # fourth circular convolution
-    for jj in range(4):
-        for ii in range(D):
-            pop = m.population(neurons_per_product)
-            m.encoder(AF[ii], pop)
-            m.encoder(BF[ii], pop)
-            m.decoder(pop, B[ii])
-
-
-for ii in range(D):
-    for jj in range(D):
-        m.transform(dummy[ii, jj], CF[ii], C[jj])
+CircularConvolution(m, A, B, neurons_per_product=3)
 
 sim = Simulator(ctx, m)
 sim.alloc_all()
