70b2cada0adc81c0ff1891fe769a97b986b532ef
James Bergstra <james.bergstra@gmail.com>
2013-08-26 15:12:55 -0400

    reworking tests

--- nengo_ocl/test/test_sim_ocl.py
+++ nengo_ocl/test/test_sim_ocl.py
@@ -1,19 +1,35 @@
 import pyopencl as cl
 
-from nengo_ocl import sim_ocl
-import nengo.test.test_old_api
-
-ctx = cl.create_some_context()
-
-
-class UseSimOcl(object):
-    def Simulator(self, *args, **kwargs):
-        rval = sim_ocl.Simulator(ctx, *args, **kwargs)
-        rval.alloc_all()
-        rval.plan_all()
-        return rval
+#from nengo_ocl import sim_ocl
+#import nengo.test.test_old_api
+from nengo.tests.helpers import simulator_test_cases
+import nengo.simulator
 
 
-class TestOldAPI(UseSimOcl, nengo.test.test_old_api.TestOldAPI):
-    show = False
+
+for TestCase in simulator_test_cases:
+    class MyTestCase(TestCase):
+        simulator_test_case_ignore = True
+        def Simulator(self, model):
+            return nengo.simulator.Simulator(model)
+    MyTestCase.__name__ = TestCase.__name__
+    globals()[TestCase.__name__] = MyTestCase
+    del MyTestCase
+    del TestCase
+
+
+if 0:
+    ctx = cl.create_some_context()
+
+
+    class UseSimOcl(object):
+        def Simulator(self, *args, **kwargs):
+            rval = sim_ocl.Simulator(ctx, *args, **kwargs)
+            rval.alloc_all()
+            rval.plan_all()
+            return rval
+
+
+    class TestOldAPI(UseSimOcl, nengo.test.test_old_api.TestOldAPI):
+        show = False
 
