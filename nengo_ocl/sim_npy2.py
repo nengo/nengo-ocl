f894145919e3cb870111ac15081e5fc0271738f2
James Bergstra <james.bergstra@gmail.com>
2013-08-28 16:43:06 -0400

    overhaul: sim_npy2 running again

--- nengo_ocl/sim_npy2.py
+++ nengo_ocl/sim_npy2.py
@@ -129,7 +129,6 @@ class ViewBuilder(object):
             names=self.names)
 
 
-
 class Simulator(object):
 
     def RaggedArray(self, *args, **kwargs):
@@ -141,6 +140,78 @@ class Simulator(object):
             [np.zeros(ss.shape) + getattr(ss, 'value', np.zeros(ss.shape))
                 for ss in sigseq],
             names=[getattr(ss, 'name', '') for ss in sigseq])
+
+    def sig_gemv(self, seq, alpha, A_js_fn, X_js_fn, beta, Y_sig_fn,
+                 Y_in_sig_fn=None):
+        sidx = self.builder.sidx
+        Y_sigs = [Y_sig_fn(item) for item in seq]
+        if Y_in_sig_fn is None:
+            Y_in_sigs = Y_sigs
+        else:
+            Y_in_sigs = [Y_in_sig_fn(item) for item in seq]
+        Y_idxs = [sidx[sig] for sig in Y_sigs]
+        Y_in_idxs = [sidx[sig] for sig in Y_in_sigs]
+
+        # -- The following lines illustrate what we'd *like* to see...
+        #
+        # A_js = self.RaggedArray(
+        #   [[sidx[ss] for ss in A_js_fn(item)] for item in seq])
+        # X_js = self.RaggedArray(
+        #   [[sidx[ss] for ss in X_js_fn(item)] for item in seq])
+        #
+        # -- ... but the simulator supports broadcasting. So in fact whenever
+        #    a signal in X_js has shape (N, 1), the corresponding A_js signal
+        #    can have shape (M, N) or (1, 1).
+        #    Fortunately, scalar multiplication of X by A can be seen as
+        #    Matrix multiplication of A by X, so we can still use gemv,
+        #    we just need to reorder and transpose.
+        A_js = []
+        X_js = []
+        for ii, item in enumerate(seq):
+            A_js_i = []
+            X_js_i = []
+            A_sigs_i = A_js_fn(item)
+            X_sigs_i = X_js_fn(item)
+            assert len(A_sigs_i) == len(X_sigs_i)
+            ysig = Y_sigs[ii]
+            yidx = Y_idxs[ii]
+            yM = self.all_data.shape0s[yidx]
+            yN = self.all_data.shape1s[yidx]
+            for asig, xsig in zip(A_sigs_i, X_sigs_i):
+                aidx = sidx[asig]
+                xidx = sidx[xsig]
+                aM = self.all_data.shape0s[aidx]
+                aN = self.all_data.shape1s[aidx]
+                xM = self.all_data.shape0s[xidx]
+                xN = self.all_data.shape1s[xidx]
+                if aN == aM == 1:
+                    A_js_i.append(xidx)
+                    X_js_i.append(aidx)
+                else:
+                    if aN != xM:
+                        raise ValueError('shape mismatch in sig_gemv',
+                                         ((asig, aM, aN),
+                                          (xsig, xM, xN),
+                                          (ysig, yM, yN),
+                                         ))
+                    A_js_i.append(aidx)
+                    X_js_i.append(xidx)
+
+            A_js.append(A_js_i)
+            X_js.append(X_js_i)
+
+
+        return ragged_gather_gemv(
+            Ms=self.all_data.shape0s,
+            Ns=self.all_data.shape1s,
+            alpha=alpha,
+            A=self.all_data, A_js=A_js,
+            X=self.all_data, X_js=X_js,
+            beta=beta,
+            Y=self.all_data[Y_idxs],
+            Y_in=self.all_data[Y_in_idxs],
+            )
+
 
     @staticmethod
     def orig_relevant_signals(model):
@@ -201,10 +272,11 @@ class Simulator(object):
             yield (enc.pop.input_signal)
 
         for dec in model.decoders:
-            yield (dec.weights_signal)
-            yield (dec.sig)
-            yield (dec.pop.input_signal)
-            yield self.dec_outputs[dec]
+            yield dec.weights_signal
+            yield dec.sig
+            yield dec.pop.input_signal
+        for sig in self.dec_outputs:
+            yield sig
 
         for sig in self.outbufs:
             yield self.outbufs[sig]
@@ -218,13 +290,15 @@ class Simulator(object):
         self.sidx = []
         self.sim_step = 0
 
-        self.nl_outbuf = {} # similar to "signals_tmp" in ref impl.
         self.lif_voltage = {}
         self.lif_reftime = {}
         self.dec_outputs = {}
         self.filter_outputs = {}    # -- values also in outbufs
         self.transform_outputs = {} # -- values also in outbufs
         self.outbufs = {}
+
+        self.probe_output = dict((probe, []) for probe in model.probes)
+
 
         # create at least one buffer for every signal
         bases = stable_unique(baseof(sig) for sig in
@@ -241,24 +315,25 @@ class Simulator(object):
         #    encoders -> input current can be done in-place
         #    nl outputs -> generally needs buffer
         for nl in model.nonlinearities:
-            # -- TODO: figure out when an output buffer is unnecessary
-            #          hint: the decoder is 1
-            self.nl_outbuf[nl] = add_base_like(nl.output_signal)
 
             # -- also some neuron models need a few extra buffers
             if isinstance(nl, Direct):
                 pass
             elif isinstance(nl, LIF):
                 self.lif_voltage[nl] = add_base_like(nl.output_signal)
-                self.lif_reftime[nl] = add_signal(nl.output_signal)
+                self.lif_reftime[nl] = add_base_like(nl.output_signal)
             elif isinstance(nl, LIFRate):
                 pass 
             else:
                 raise NotImplementedError()
 
         #    decoder outputs -> also generally needs copy
-        for dec in model.decoders:
-            self.dec_outputs[dec] = add_base_like(dec.pop.output_signal)
+        decoder_outputs_set = set(dec.sig for dec in model.decoders)
+        self.decoder_outputs = [sig
+                           for sig in self.orig_relevant_signals(self.model)
+                           if sig in decoder_outputs_set]
+        for sig in self.decoder_outputs:
+            self.dec_outputs[sig] = add_base_like(sig)
 
         #    generally, filters and transforms are meant to
         #    write into a fresh "output buffer"
@@ -284,7 +359,7 @@ class Simulator(object):
         self.bases[:] = bases
         self.all_data = self.alloc_signal_data(self.bases)
         self.builder = ViewBuilder(self.bases, self.all_data)
-        for sig in self.signals_iter():
+        for sig in stable_unique(self.signals_iter()):
             self.builder.append_view(sig)
         self.builder.add_views_to(self.all_data)
 
@@ -297,17 +372,15 @@ class Simulator(object):
         Combine the elements of input accumulator buffer (sigs_ic)
         into *add* them into sigs
         """
-        # ADD linear combinations of signals to some signals
-        ragged_gather_gemv(
-            Ms=self.all_data_A.shape0s,
-            Ns=self.all_data_A.shape1s,
-            alpha=1.0,
-            A=self.all_data_B,
-            A_js=self.tf_A_js,
-            X=self.all_data_B,
-            X_js=self.tf_X_js,
-            beta=1.0,
-            Y=self.tf_Y,
+        transforms = self.model.transforms
+        return self.sig_gemv(self.outbufs.keys(),
+            1.0,
+            lambda sig: [tf.alpha_signal
+                         for tf in transforms if tf.outsig == sig],
+            lambda sig: [tf.insig
+                         for tf in transforms if tf.outsig == sig],
+            1.0,
+            lambda sig: self.outbufs[sig],
             )
 
     def do_filters(self):
@@ -315,95 +388,106 @@ class Simulator(object):
         Recombine the elements of previous signal buffer (sigs)
         and write them back to `sigs`
         """
-        # ADD linear combinations of signals to some signals
-        ragged_gather_gemv(
-            Ms=self.all_data_A.shape0s,
-            Ns=self.all_data_A.shape1s,
-            alpha=1.0,
-            A=self.all_data_A,
-            A_js=self.f_A_js,
-            X=self.all_data_A,
-            X_js=self.f_X_js,
-            beta=0.0,
-            Y=self.f_Y,
+        filters = self.model.filters
+        return self.sig_gemv(
+            self.outbufs.keys(),
+            1.0,
+            lambda sig: [tf.alpha_signal
+                         for tf in filters if tf.newsig == sig],
+            lambda sig: [tf.oldsig
+                         for tf in filters if tf.newsig == sig],
+            0.0,
+            lambda sig: self.outbufs[sig],
             )
 
     def do_encoders(self):
         encoders = self.model.encoders
-        sidx = self.builder.sidx
 
-        # -- which encoder(s) does each population use
-        self.enc_A_js = self.RaggedArray([
-            [sidx[enc.weights_signal] for enc in encoders if enc.pop == pop]
-            for pop in self.model.nonlinearities])
-
-        # -- and which corresponding signal(s) does it encode
-        self.enc_X_js = self.RaggedArray([
-            [sidx[enc.sig] for enc in encoders if enc.pop == pop]
-            for pop in self.model.nonlinearities])
-
-        enc_bias_idxs = [sidx[pop.bias_signal]
-                for pop in self.model.nonlinearities]
-        enc_out_idxs = [sidx[pop.input_signal]
-                for pop in self.model.nonlinearities]
-        self.enc_Y_in = self.all_data[enc_bias_idxs]
-        self.enc_Y = self.all_data[enc_out_idxs]
-
-        ragged_gather_gemv(
-            Ms=self.all_data.shape0s,
-            Ns=self.all_data.shape1s,
-            alpha=1.0,
-            A=self.all_data, A_js=self.enc_A_js,
-            X=self.all_data, X_js=self.enc_X_js,
-            beta=1.0,
-            Y_in=self.enc_Y_in,
-            Y=self.enc_Y,
+        return self.sig_gemv(
+            self.model.nonlinearities,
+            1.0,
+            lambda pop: [enc.weights_signal
+                         for enc in encoders if enc.pop == pop],
+            lambda pop: [enc.sig
+                         for enc in encoders if enc.pop == pop],
+            1.0,
+            lambda pop: pop.input_signal,
+            lambda pop: pop.bias_signal,
             )
 
     def do_populations(self):
         dt = self.model.dt
+        sidx = self.builder.sidx
         nls = sorted(self.model.nonlinearities, key=type)
         for nl_type, nl_group in itertools.groupby(nls, type):
-            print 'TODO', nl_type, list(nl_group)
+            if nl_type == Direct:
+                for nl in nl_group:
+                    J = self.all_data[sidx[nl.input_signal]]
+                    output = nl.fn(J)
+                    self.all_data[sidx[nl.output_signal]][:] = output
+            elif nl_type == LIF:
+                for nl in nl_group:
+                    J = self.all_data[sidx[nl.input_signal]]
+                    voltage = self.all_data[sidx[self.lif_voltage[nl]]]
+                    reftime = self.all_data[sidx[self.lif_reftime[nl]]]
+                    output = self.all_data[sidx[nl.output_signal]]
+                    nl.step_math0(dt, J, voltage, reftime, output,)
+            else:
+                raise NotImplementedError(nl_type)
 
     def do_decoders(self):
-        ragged_gather_gemv(
-            Ms=self.all_data_A.shape0s,
-            Ns=self.all_data_A.shape1s,
-            alpha=1.0,
-            A=self.all_data_B,
-            A_js=self.dec_A_js,
-            X=self.all_data_B,
-            X_js=self.dec_X_js,
-            beta=0.0,
-            Y=self.dec_Y,
+        decoders = self.model.decoders
+
+        return self.sig_gemv(
+            self.decoder_outputs,
+            1.0, 
+            lambda sig: [dec.weights_signal
+                         for dec in decoders if dec.sig == sig],
+            lambda sig: [dec.pop.output_signal
+                         for dec in decoders if dec.sig == sig],
+            0.0,
+            lambda sig: sig,
             )
 
     def do_probes(self):
-        for period in self.probes_by_period:
-            if 0 == self.sim_step % period:
-                for probe in self.probes_by_period[period]:
-                    val = self.all_signal_idxs[probe.sig]
-                    self.probe_output[probe].append(
-                        self.all_data_B[val].copy())
+        probes = self.model.probes
+        sidx = self.builder.sidx
+        for probe in probes:
+            period = int(probe.dt // self.model.dt)
+            if self.sim_step % period == 0:
+                self.probe_output[probe].append(
+                    self.all_data[sidx[probe.sig]].copy()
+                    )
+
+
+    def do_back_copy(self):
+        return self.sig_gemv(
+            self.outbufs.keys(),
+            1.0,
+            lambda sig: [self.model.one],
+            lambda sig: [self.outbufs[sig]],
+            0.0,
+            lambda sig: sig,
+            )
 
     def do_all(self):
         #print '-' * 10 + 'A' * 10 + '-' * 10
         #print self.all_data_A
         #print '-' * 20
-        self.do_encoders()          # encode sigs_t into pops
+        self.do_encoders()
         #print self.pop_J
-        self.do_populations()       # pop dynamics
+        self.do_populations()
         #print self.pop_J
-        self.do_decoders()          # decode into sigs_ic
+        self.do_decoders()
         #print self.pop_J
-        self.do_filters()           # make sigs_t's contribution to t + 1
+        self.do_filters()
         #print self.pop_J
-        self.do_transforms()        # add sigs_ic's contribution to t + 1
+        self.do_transforms()
+        #print self.pop_J
+        self.do_back_copy()
         #print self.pop_J
         self.do_probes()
         #print self.pop_J
-        self.all_data_A.buf[:] = self.all_data_B.buf
 
         #print '-' * 10 + 'B' * 10 + '-' * 10
         #print self.all_data_A
