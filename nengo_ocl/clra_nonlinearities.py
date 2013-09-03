1bb26fde768f16fafd2f16421609600ef5082d00
hunse <hunse@ctn>
2013-09-03 13:02:10 -0400

    The LIF nonlinearity is now fully working.
    - I've added a helper function that should make it easy
      to add more nonlinearities.
    - This helper function allows for either one kernel per computation
      or one kernel doing a few computations.
    - The only thing left to add to the LIF is upsampling.

--- nengo_ocl/clra_nonlinearities.py
+++ nengo_ocl/clra_nonlinearities.py
@@ -4,6 +4,7 @@ import pyopencl as cl
 from plan import Plan
 from mako.template import Template
 from clarray import to_device
+from .clraggedarray import CLRaggedArray
 
 def equal_arrays(a, b):
     return (np.asarray(a) == np.asarray(b)).all()
@@ -11,121 +12,247 @@ def equal_arrays(a, b):
 def plan_lif(queue, J, V, W, OV, OW, OS, ref, tau, dt, tag=None):
     """
     """
+    inputs = dict(j=J, v=V, w=W)
+    outputs = dict(ov=OV, ow=OW, os=OS)
+    parameters = dict(tau=tau, ref=ref)
 
-    ### EH: ideas for speed
-    ### - make separate inline function for each population, switch statement
-    ### - flatten grid, so we don't have so many returning kernel calls
+    textconf = dict(dt=dt, dt_inv=1. / dt, V_threshold=1.)
 
-    V_threshold = 1.
-
-    N = len(J)
-
-    textconf = {
-        'ref': ref,
-        'tau': tau,
-        'dt': dt,
-        'dt_inv': 1. / dt,
-        'V_threshold': V_threshold,
-        'tag': str(tag),
-    }
-
-
-    for vname in ['J', 'V', 'W', 'OV', 'OW', 'OS']:
-        v = locals()[vname]
-        textconf[vname + 'type'] = v.cl_buf.ocldtype
-        textconf[vname + 'offset'] = '%(name)s_starts[n] + m' % {'name': vname}
-        assert len(v) == N
-        assert equal_arrays(v.shape0s, J.shape0s)
-
-        ### N.B. - we should be able to ignore ldas as long as all vectors
-        assert equal_arrays(v.shape1s, 1)
+    declares = """
+            char spiked;
+            %(Vtype)s dV, overshoot;
+            """ % ({'Vtype': V.cl_buf.ocldtype})
 
     text = """
-        __kernel void lif_step(
-            __global const int *lengths,
-            __global const int *J_starts,
-            __global const ${Jtype} *J,
-            __global const int *V_starts,
-            __global const ${Vtype} *V,
-            __global const int *W_starts,
-            __global const ${Wtype} *W,
-            __global const int *OV_starts,
-            __global ${Vtype} *OV,
-            __global const int *OW_starts,
-            __global ${Wtype} *OW,
-            __global const int *OS_starts,
-            __global ${OStype} *OS
-                     )
-        {
-            const int m = get_global_id(0);
-            const int n = get_global_id(1);
-            const int M = lengths[n];
-            if (m >= M) return;
+            spiked = 0;
 
-            ${Jtype} j = J[${Joffset}];
-            ${Vtype} v = V[${Voffset}];
-            ${Wtype} w = W[${Woffset}];
-
-            char spiked = 0;
-            ${Vtype} dV, overshoot;
-            ${Wtype} post_ref, spiketime;
-
-            dV = (${dt} / ${tau}) * (j - v);
+            dV = (${dt} / tau) * (j - v);
             v += dV;
 
-            post_ref = 1.0 - (w - ${dt}) * ${dt_inv};
-            v = v > 0 ?
-                v * (post_ref < 0 ? 0.0 : post_ref < 1 ? post_ref : 1.0)
-                : 0;
+            if (v < 0 || w > 2*${dt})
+                v = 0;
+            else if (w > ${dt})
+                v *= 1.0 - (w - ${dt}) * ${dt_inv};
+
             spiked = v > ${V_threshold};
             if (v > ${V_threshold}) {
-                overshoot = (v - ${V_threshold}) / dV;
-                spiketime = ${dt} * (1.0f - overshoot);
-                w = ${ref} + spiketime;
+                overshoot = ${dt} * (v - ${V_threshold}) / dV;
+                w = ref - overshoot + ${dt};
                 v = 0.0;
             } else {
                 w -= ${dt};
             }
 
-            OV[${OVoffset}] = v;
-            OW[${OWoffset}] = w;
-            OS[${OSoffset}] = spiked ? 1.0f : 0.0f;
+            ov = v;
+            ow = w;
+            os = (spiked) ? 1.0f : 0.0f;
+            """
+    text = Template(text, output_encoding='ascii').render(**textconf)
+
+    return _plan_template(
+        queue, "lif_step", text, declares=declares, tag=tag, n_elements=0,
+        inputs=inputs, outputs=outputs, parameters=parameters)
+
+def _plan_template(queue, name, core_text, declares="", tag=None, n_elements=0,
+                   inputs={}, outputs={}, parameters={}):
+    """Template for making a plan for vector nonlinearities.
+
+    This template assumes that all inputs and outputs are vectors.
+
+    Parameters
+    ----------
+    n_elements: int
+        If n_elements == 0, then the kernels are allocated as a block. This is
+        simple, but can be slow for large computations where input vector sizes
+        are not uniform (e.g. one large population and many small ones).
+        If n_elements >= 1, then all the vectors in the RaggedArray are
+        flattened so that the exact number of required kernels is allocated.
+        Each kernel performs computations for `n_elements` elements.
+
+    inputs: dictionary of CLRaggedArrays
+        Inputs to the function. RaggedArrays must be a list of vectors.
+
+    outputs: dictionary of CLRaggedArrays
+        Outputs of the function. RaggedArrays must be a list of vectors.
+
+    parameters: dictionary of CLRaggedArrays
+        Parameters to the function. RaggedArrays must be a list of vectors.
+        Providing a float instead of a RaggedArray makes that parameter
+        constant.
+
+    """
+
+    ### EH: ideas for speed
+    ### - make separate inline function for each population, switch statement
+    ### - flatten grid, so we don't have so many returning kernel calls
+
+    base = inputs.values()[0]   # input to use as reference (for lengths)
+    N = len(base)
+
+    ### split parameters into static and updated params
+    sparams = {}  # static params (hard-coded)
+    uparams = {}  # variable params (updated)
+    for k, v in parameters.items():
+        if isinstance(v, CLRaggedArray):
+            uparams[k] = v
+        else:
+            try:
+                sparams[k] = ('float', float(v))
+            except TypeError:
+                raise
+
+    ivars = dict(inputs.items() + uparams.items())
+    ovars = dict(outputs)
+    avars = ivars.items() + ovars.items()
+
+    variables = {}
+    for vname, v in avars:
+        assert vname not in variables, "Name clash"
+        assert len(v) == N
+        assert equal_arrays(v.shape0s, base.shape0s)
+
+        ### N.B. - we should be able to ignore ldas as long as all vectors
+        assert equal_arrays(v.shape1s, 1)
+
+        dtype = v.cl_buf.ocldtype
+        offset = '%(name)s_starts[n]' % {'name': vname}
+        variables[vname] = (dtype, offset)
+
+    ivariables = dict((k, variables[k]) for k in ivars.keys())
+    ovariables = dict((k, variables[k]) for k in ovars.keys())
+
+    textconf = dict(N=N, n_elements=n_elements, tag=str(tag),
+                    declares=declares, core_text=core_text,
+                    variables=variables, sparams=sparams,
+                    ivariables=ivariables, ovariables=ovariables)
+
+    text = """
+        ////////// MAIN FUNCTION //////////
+        __kernel void fn(
+% for name, [type, offset] in ivariables.items():
+            __global const int *${name}_starts,
+            __global const ${type} *in_${name},
+% endfor
+% for name, [type, offset] in ovariables.items():
+            __global const int *${name}_starts,
+            __global ${type} *in_${name},
+% endfor
+            __global const int *lengths
+        )
+        {
+
+% if n_elements > 0:
+            const int gid = get_global_id(0);
+            int m = gid * ${n_elements}, n = 0;
+            while (m >= lengths[n]) {
+                m -= lengths[n];
+                n++;
+            }
+            if (n >= ${N}) return;
+
+% for name, [type, offset] in ivariables.items():
+            __global const ${type} *cur_${name} = in_${name} + ${offset} + m;
+% endfor
+% for name, [type, offset] in ovariables.items():
+            __global ${type} *cur_${name} = in_${name} + ${offset} + m;
+% endfor
+% for name, [type, offset] in variables.items():
+            ${type} ${name};
+% endfor
+% else:
+            const int m = get_global_id(0);
+            const int n = get_global_id(1);
+            const int M = lengths[n];
+            if (m >= M) return;
+
+% for name, [type, offset] in ivariables.items():
+            ${type} ${name} = in_${name}[${offset} + m];
+% endfor
+% for name, [type, offset] in ovariables.items():
+            ${type} ${name};
+% endfor
+% endif
+
+% for name, [type, value] in sparams.items():
+            const ${type} ${name} = ${value};
+% endfor
+            //////////////////////////////////////////////////
+            //vvvvv USER DECLARATIONS BELOW vvvvv
+            ${declares}
+            //^^^^^ USER DECLARATIONS ABOVE ^^^^^
+            //////////////////////////////////////////////////
+
+% if n_elements > 0:
+  % for ii in range(n_elements):
+            //////////////////////////////////////////////////
+            ////////// LOOP ITERATION ${ii}
+    % for name, [type, offset] in ivariables.items():
+            ${name} = *cur_${name};
+    % endfor
+
+            /////vvvvv USER COMPUTATIONS BELOW vvvvv
+            ${core_text}
+            /////^^^^^ USER COMPUTATIONS ABOVE ^^^^^
+
+    % for name, [type, offset] in ovariables.items():
+            *cur_${name} = ${name};
+    % endfor
+
+    % if ii + 1 < n_elements:
+            m++;
+            if (m >= lengths[n]) {
+                n++;
+                m = 0;
+                if (n >= ${N}) return;
+
+      % for name, [type, offset] in variables.items():
+                cur_${name} = in_${name} + ${offset};
+      % endfor
+            } else {
+      % for name, [type, offset] in variables.items():
+                cur_${name}++;
+      % endfor
+            }
+    % endif
+
+  % endfor
+% else:
+            /////vvvvv USER COMPUTATIONS BELOW vvvvv
+            ${core_text}
+            /////^^^^^ USER COMPUTATIONS ABOVE ^^^^^
+
+  % for name, [type, offset] in ovariables.items():
+            in_${name}[${offset} + m] = ${name};
+  % endfor
+% endif
         }
         """
-
     text = Template(text, output_encoding='ascii').render(**textconf)
-    # for i, line in enumerate(text.split('\n')):
-    #     print "%2d %s" % (i, line)
+    if 0:
+        for i, line in enumerate(text.split('\n')):
+            print "%3d %s" % (i + 1, line)
 
-    gsize = (int(max(J.shape0s)), int(N))
+    if n_elements > 0:
+        gsize = (int(np.ceil(np.sum(base.shape0s) / float(n_elements))),)
+    else:
+        gsize = (int(np.max(base.shape0s)), int(N))
     lsize = None
-    _fn = cl.Program(queue.context, text).build().lif_step
-    full_args = (
-        J.cl_shape0s,
-        J.cl_starts,
-        J.cl_buf,
-        V.cl_starts,
-        V.cl_buf,
-        W.cl_starts,
-        W.cl_buf,
-        OV.cl_starts,
-        OV.cl_buf,
-        OW.cl_starts,
-        OW.cl_buf,
-        OS.cl_starts,
-        OS.cl_buf
-        )
+    _fn = cl.Program(queue.context, text).build().fn
 
-    #print [str(arr.dtype)[0] for arr in full_args]
+    full_args = []
+    for name, v in avars:
+        full_args.extend([v.cl_starts, v.cl_buf])
+    full_args.append(base.cl_shape0s)
+    full_args = tuple(full_args)
+
     _fn.set_args(*[arr.data for arr in full_args])
     rval = Plan(queue, _fn, gsize, lsize,
-                name='lif_step',
+                name=name,
                 tag=tag,
                )
     # prevent garbage-collection
     rval.full_args = full_args
     return rval
-
 
 
 # from mako.template import Template
--- nengo_ocl/test/test_clra_nonlinearities.py
+++ nengo_ocl/test/test_clra_nonlinearities.py
@@ -23,22 +23,24 @@ def test_lif_step():
     # t = dt * np.arange(np.round(t_final / dt))
     # nt = len(t)
 
-    ref = 2e-3
-    tau = 20e-3
-
     # n_neurons = [3, 3, 3]
     # n_neurons = [500, 200, 100]
-    n_neurons = [50000, 40000, 30000]
+    n_neurons = [12345, 23456, 34567]
     J = RA([np.random.randn(n) for n in n_neurons])
     V = RA([np.random.uniform(low=0, high=1, size=n) for n in n_neurons])
     W = RA([np.random.uniform(low=-10*dt, high=10*dt, size=n) for n in n_neurons])
     OS = RA([np.zeros(n) for n in n_neurons])
+
+    ref = 2e-3
+    tau = 20e-3
+    # tau_array = RA([tau*np.ones(n) for n in n_neurons])
 
     queue = cl.CommandQueue(ctx)
     dJ = CLRA(queue, J)
     dV = CLRA(queue, V)
     dW = CLRA(queue, W)
     dOS = CLRA(queue, OS)
+    # dTau = CLRA(queue, tau_array)
 
     ### simulate host
     nls = [LIF(n, tau_ref=ref, tau_rc=tau) for n in n_neurons]
@@ -47,28 +49,25 @@ def test_lif_step():
 
     ### simulate device
     plan = plan_lif(queue, dJ, dV, dW, dV, dW, dOS, ref, tau, dt)
+    # plan = plan_lif(queue, dJ, dV, dW, dV, dW, dOS, ref, dTau, dt)
     plan()
+
+    if 1:
+        a, b = V, dV
+        for i in xrange(len(a)):
+            nc, _ = not_close(a[i], b[i]).nonzero()
+            if len(nc) > 0:
+                j = nc[0]
+                print "i", i, "j", j
+                print "J", J[i][j], dJ[i][j]
+                print "V", V[i][j], dV[i][j]
+                print "W", W[i][j], dW[i][j]
+                print "...", len(nc) - 1, "more"
 
     assert ra.allclose(J, dJ.to_host())
     assert ra.allclose(V, dV.to_host())
     assert ra.allclose(W, dW.to_host())
-    # if not ra.allclose(W, dW.to_host()):
-    #     for i in xrange(len(W)):
-    #         nc, _ = not_close(W[i], dW[i]).nonzero()
-    #         j = nc[0]
-    #         print "J", J[i][j], dJ[i][j]
-    #         print "V", V[i][j], dV[i][j]
-    #         print "W", W[i][j], dW[i][j]
-    #     assert 0
-
     assert ra.allclose(OS, dOS.to_host())
-
-    import time
-    timer = time.time()
-    for i in xrange(10):
-        plan()
-    print
-    print "elapsed time:", time.time() - timer
 
 def test_lif_speed():
     # import time
@@ -97,6 +96,8 @@ def test_lif_speed():
     dOS = CLRA(queue, OS)
 
     plan = plan_lif(queue, dJ, dV, dW, dV, dW, dOS, ref, tau, dt)
+    # plan = plan_lif_flat(queue, dJ, dV, dW, dV, dW, dOS, ref, tau, dt)
+    # plan = plan_lif_group(queue, dJ, dV, dW, dV, dW, dOS, ref, tau, dt)
 
     for i in range(1000):
         plan(profiling=True)
