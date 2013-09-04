46241716e6bc69a414dca50835ae0e37dfd9a6bb
James Bergstra <james.bergstra@gmail.com>
2013-09-04 16:31:12 -0400

    include @zero-impact's reduction code

--- nengo_ocl/clra_gemv.py
+++ nengo_ocl/clra_gemv.py
@@ -4,6 +4,163 @@ from plan import Plan
 from mako.template import Template
 from clarray import to_device
 
+def plan_parallel_ragged_gather_gemv2(queue,
+        alpha, A, A_js, X, X_js,
+        beta, Y, group_size = 32, Y_in=None, tag=None):
+    """
+    """
+
+    # TODO: if alpha or beta is a float
+    #       then render it into the kernel text.
+    try:
+        alpha = float(alpha)
+        cl_alpha = None
+    except TypeError:
+        cl_alpha = to_device(queue,
+            np.asarray([alpha] * len(Y), Y.buf.dtype))
+
+    try:
+        beta = float(beta)
+        cl_beta = None
+    except TypeError:
+        cl_beta = to_device(queue,
+            np.asarray([beta] * len(Y), Y.buf.dtype))
+
+    if cl_alpha or cl_beta:
+        raise NotImplementedError()
+
+    if Y_in is None:
+        Y_in = Y
+
+    # XXX check that all the ints are ints not longs
+    textconf = {
+        'alpha' : str(alpha),
+        'beta' : str(beta),
+        'type_alpha': None, #cl_alpha.ocldtype,
+        'type_beta': None, #cl_beta.ocldtype,
+        'type_A': A.cl_buf.ocldtype,
+        'type_X': X.cl_buf.ocldtype,
+        'type_Y': Y.cl_buf.ocldtype,
+        'y_len': len(Y),
+        'lsize': group_size,
+    }
+
+    text = """
+        __kernel void fn(
+            //const __global ${type_alpha} * alphas,
+            const __global int *A_starts,
+            const __global int *A_shape1s,
+            const __global ${type_A} *A_data,
+            const __global int *A_js_starts,
+            const __global int *A_js_lens,
+            const __global int *A_js_data,
+            const __global int *X_starts,
+            const __global ${type_X} *X_data,
+            const __global int *X_js_starts,
+            const __global int *X_js_data,
+            //const __global ${type_beta} * betas,
+            const __global int *Y_in_starts,
+            const __global ${type_Y} *Y_in_data,
+            const __global int *Y_starts,
+            const __global int *Y_lens,
+            __global ${type_Y} *Y_data)
+        {
+            //const int mm = get_global_id(1); //TODO
+
+            __local ${type_Y} partialDotProduct[${lsize}]; //Scratch space for the dot products
+
+            //Y is divided into groups of size group_size. Each work-item does enough dot-products to cover one of the groups
+            for (uint yi = get_group_id(0); yi < ${y_len}; yi += get_num_groups(0)) {
+
+                const __global int* X_js_row = X_js_data + X_js_starts[yi];
+                const __global int* A_js_row = A_js_data + A_js_starts[yi];
+
+                //const ${type_alpha} alpha = alphas[yi];
+                //const ${type_beta} beta = betas[yi];
+
+                int y_offset = Y_starts[yi];
+                int y_in_offset = Y_in_starts[yi];
+                % if beta != 0 :
+                    Y_data[y_offset] = ${beta} * Y_in_data[y_in_offset];
+                % else :
+                    Y_data[y_offset] = 0;
+                % endif
+
+                float sum = 0;
+                int n_dot_products = A_js_lens[yi]; //Do all of xjs dot products at same time
+                for(int j = 0; j < n_dot_products; j++) {
+                    const int x_ji = X_js_row[j];
+                    const int a_ji = A_js_row[j];
+                    const int N_i = A_shape1s[a_ji];
+
+                    const __global ${type_A}* A_row = A_data + A_starts[a_ji]; //Get the rows for the product
+                    const __global ${type_X}* X_row = X_data + X_starts[x_ji];
+
+                    //Each work item will do some fraction of the multiplications and store the result locally
+                    for (uint x = get_local_id(0); x < N_i; x += get_local_size(0)) {
+                        sum += A_row[x] * X_row[x];
+                    }
+                }
+                partialDotProduct[get_local_id(0)] = sum;
+
+                //Parallel reduction of locally stored sums
+                for (uint stride = 1; stride < get_local_size(0); stride *= 2) {
+                    barrier(CLK_LOCAL_MEM_FENCE);
+
+                    uint index = 2 * stride * get_local_id(0);
+                    if (index < get_local_size(0)) {
+                        partialDotProduct[index] += partialDotProduct[index + stride];
+                    }
+                }
+
+                //Multiply by alpha and store the result.
+                if (get_local_id(0) == 0) {
+                    Y_data[yi] += ${alpha} * partialDotProduct[0];
+                    barrier(CLK_LOCAL_MEM_FENCE);
+                }
+            }
+        }
+        """
+
+    text = Template(text, output_encoding='ascii').render(**textconf)
+
+    # Make the global size the closest multiple of the group size (ceiling)
+    y_size = int(np.ceil(len(Y) / float(group_size))) * group_size
+    gsize = (y_size,)
+    lsize = (group_size,)
+
+    _fn = cl.Program(queue.context, text).build().fn
+
+    full_args = (
+                 #cl_alpha,
+                 A.cl_starts,
+                 A.cl_shape1s,
+                 A.cl_buf,
+                 A_js.cl_starts,
+                 A_js.cl_shape0s,
+                 A_js.cl_buf,
+                 X.cl_starts,
+                 X.cl_buf,
+                 X_js.cl_starts,
+                 X_js.cl_buf,
+                 #cl_beta,
+                 Y_in.cl_starts,
+                 Y_in.cl_buf,
+                 Y.cl_starts,
+                 Y.cl_shape0s,
+                 Y.cl_buf,
+                )
+
+    _fn.set_args(*[arr.data for arr in full_args])
+
+    rval = Plan(queue, _fn, gsize, lsize,
+                name='ref_parallel_ragged_gather_gemv',
+                tag=tag,
+               )
+    # prevent garbage-collection
+    rval.full_args = full_args
+    return rval
+
 
 def plan_ragged_gather_gemv(queue, alpha, A, A_js, X, X_js,
                             beta, Y, Y_in=None, tag=None):
--- nengo_ocl/test/test_clra_gemv.py
+++ nengo_ocl/test/test_clra_gemv.py
@@ -1,86 +1,146 @@
+from nengo_ocl.tricky_imports import unittest
+
 import nose
 import numpy as np
-from ..ra_gemv import ragged_gather_gemv
-from  .. import raggedarray as ra
+from nengo_ocl.ra_gemv import ragged_gather_gemv
+from  nengo_ocl import raggedarray as ra
 RA = ra.RaggedArray
-from ..clraggedarray import CLRaggedArray as CLRA
+from nengo_ocl.clraggedarray import CLRaggedArray as CLRA
 
-from ..clra_gemv import plan_ragged_gather_gemv
+from nengo_ocl.clra_gemv import plan_ragged_gather_gemv
+from nengo_ocl.clra_gemv import plan_parallel_ragged_gather_gemv2
 
 import pyopencl as cl
 ctx = cl.create_some_context()
 
-def test_basic():
-    # -- prepare initial conditions on host
-    A = RA([ [[0.1, .2], [.3, .4]], [[.5, .6]]])
-    X = RA([ [3, 5] ])
+class TestStuff(unittest.TestCase):
 
-    A_js = RA([[1], [0]])
-    X_js = RA([[0], [0]])
+    def test_basic(self):
+        # -- prepare initial conditions on host
+        A = RA([ [[0.1, .2], [.3, .4]], [[.5, .6]]])
+        X = RA([ [3, 5] ])
 
-    Y = RA([[0.0], [2, 3],])
+        A_js = RA([[1], [0]])
+        X_js = RA([[0], [0]])
 
-    # -- prepare initial conditions on device
-    queue = cl.CommandQueue(ctx)
+        Y = RA([[0.0], [2, 3],])
 
-    clA = CLRA(queue, A)
-    clX = CLRA(queue, X)
+        # -- prepare initial conditions on device
+        queue = cl.CommandQueue(ctx)
 
-    clA_js = CLRA(queue, A_js)
-    clX_js = CLRA(queue, X_js)
+        clA = CLRA(queue, A)
+        clX = CLRA(queue, X)
 
-    clY = CLRA(queue, Y)
+        clA_js = CLRA(queue, A_js)
+        clX_js = CLRA(queue, X_js)
 
-    plan = plan_ragged_gather_gemv(
-        queue,
-        .5, clA, clA_js, clX, clX_js, .1, clY)
+        clY = CLRA(queue, Y)
 
-    assert ra.allclose(A, clA)
-    assert ra.allclose(X, clX)
-    assert ra.allclose(Y, clY)
-    assert ra.allclose(A_js, clA_js)
-    assert ra.allclose(X_js, clX_js)
+        plan = plan_ragged_gather_gemv(
+            queue,
+            .5, clA, clA_js, clX, clX_js, .1, clY)
 
-    # -- run host computation
-    ragged_gather_gemv(
-        .5, A, A_js, X, X_js, .1, Y,
-        use_raw_fn=True)
-    result1 = Y.buf
+        assert ra.allclose(A, clA)
+        assert ra.allclose(X, clX)
+        assert ra.allclose(Y, clY)
+        assert ra.allclose(A_js, clA_js)
+        assert ra.allclose(X_js, clX_js)
 
-    # -- run cl computation
-    plan()
-    result2 = clY.buf
+        # -- run host computation
+        ragged_gather_gemv(
+            .5, A, A_js, X, X_js, .1, Y,
+            use_raw_fn=True)
+        result1 = Y.buf
 
-    # -- ensure they match
-    print result1
-    print result2
-    assert ra.allclose(Y, clY)
+        # -- run cl computation
+        plan()
+        result2 = clY.buf
+
+        # -- ensure they match
+        print result1
+        print result2
+        assert ra.allclose(Y, clY)
 
 
-def test_reduction_speed():
-    queue = cl.CommandQueue(
-        ctx,
-        properties=cl.command_queue_properties.PROFILING_ENABLE)
+    def test_reduction_speed(self, runs=10):
+        queue = cl.CommandQueue(
+            ctx,
+            properties=cl.command_queue_properties.PROFILING_ENABLE)
 
-    L = 512  # -- length of each vector
-    N = 1000 # -- number of vectors
+        L = 512  # -- length of each vector
+        N = 1000 # -- number of vectors
 
-    A = CLRA(queue, RA([np.random.randn(1, L) for i in range(N)]))
-    X = CLRA(queue, RA([np.random.randn(L, 1) for i in range(N)]))
+        A = CLRA(queue, RA([np.random.randn(1, L) for i in range(N)]))
+        X = CLRA(queue, RA([np.random.randn(L, 1) for i in range(N)]))
 
-    X_js = CLRA(queue, RA([[i] for i in range(N) + range(N)]))
-    A_js = CLRA(queue, RA([[i] for i in range(N) + list(reversed(range(N)))]))
+        X_js = CLRA(queue,
+                RA([[i] for i in range(N) + range(N)]))
+        A_js = CLRA(queue,
+                RA([[i] for i in range(N) + list(reversed(range(N)))]))
 
-    Y = CLRA(queue, RA([[1.0] for i in range(2 * N)]))
+        Y = CLRA(queue, RA([[1.0] for i in range(2 * N)]))
 
-    plan = plan_ragged_gather_gemv(queue,
-                                   1.0, A, A_js, X, X_js,
-                                   0.0, Y)
-    for i in range(10):
-        plan(profiling=True)
+        plan = plan_ragged_gather_gemv(queue,
+                                       1.0, A, A_js, X, X_js,
+                                       0.0, Y)
+        for i in range(runs):
+            plan(profiling=True)
 
-    print 'n_calls         ', plan.n_calls
-    print 'queued -> submit', plan.atime
-    print 'submit -> start ', plan.btime
-    print 'start -> end    ', plan.ctime
+        print 'n_calls(gemv)             ', plan.n_calls
+        print 'avg. queued -> submit (ms)', (1000 * plan.atime / runs)
+        print 'avg. submit -> start  (ms)', (1000 * plan.btime / runs)
+        print 'avg. start -> end     (ms)', (1000 * plan.ctime / runs)
+
+
+    # -- Using same data and number of runs as test_reduction_speed() performs
+    #    dot-products #in groups within separate work-items.
+    #    -  group_size corresponds to the size of the separate chunks
+    #       that Y is split into.
+    #       That is, if N = 1000 and group_size = 32,
+    #       then each kernel will do 1024/32=32 dot-products
+    #       32 is optimal size on my GT650M
+    def test_parallel_reduction_speed(self,
+            group_size=16,
+            runs=100):
+        queue = cl.CommandQueue(
+            ctx,
+            properties=cl.command_queue_properties.PROFILING_ENABLE)
+
+        np.random.seed(50)
+
+        L = 512  # -- length of each vector
+        N = 1000 # -- number of vectors
+
+        # adjacency matrix for synapse weights. Row is a soma, column soma
+        A = CLRA(queue, RA([np.random.randn(1, L) for i in range(N)]))
+        X = CLRA(queue, RA([np.random.randn(L, 1) for i in range(N)]))
+        # X potential on axons. X*A = synapse strength times current strength.
+        # Dot product from row of A to row of X to get total current that
+        # goes into a soma.  List of lists of size 1.
+        # Xjs can be different lengths too!
+
+        X_js = CLRA(queue, RA([[i] for i in range(N) + range(N)]))
+        A_js = CLRA(queue, RA([[i] for i in range(N) + list(reversed(range(N)))]))
+
+        Y = CLRA(queue, RA([[1.0] for i in range(2 * N)]))
+
+        plan = plan_parallel_ragged_gather_gemv2(
+            queue,
+            1.0, A, A_js, X, X_js,
+            0.0, Y, group_size)
+
+        for i in xrange(runs):
+            plan(profiling=True)
+
+        print 'n_calls(gemv2)            ', plan.n_calls
+        print 'avg. queued -> submit (ms)', (1000 * plan.atime / runs)
+        print 'avg. submit -> start  (ms)', (1000 * plan.btime / runs)
+        print 'avg. start -> end     (ms)', (1000 * plan.ctime / runs)
+
+        #print "Output: "
+        #print Y.buf
+
+
+if __name__ == '__main__':
+   unittest.main()
 
