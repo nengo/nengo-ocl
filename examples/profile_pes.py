5b221696e0559198cfe05db28669ae953917134e
Eric Hunsberger <erichuns@gmail.com>
2017-08-10 15:14:30 -0400

    Added profiling script for PES learning rule
    Good for testing some of the basic ops used in learning,
    for example the changes to the copy op.

---
+++ examples/profile_pes.py
@@ -0,0 +1,47 @@
+import time
+
+import numpy as np
+import pyopencl as cl
+
+import nengo
+from nengo.networks.circularconvolution import circconv, transform_in
+
+import nengo_ocl
+
+ctx = cl.create_some_context()
+
+dims = 16
+# neurons_per_dim = 64
+neurons_per_dim = 256
+# neurons_per_dim = 1024
+n_neurons = dims * neurons_per_dim
+
+r = 1e-4
+W0 = np.random.uniform(-r, r, size=(n_neurons, n_neurons))
+
+runtime = 1.
+
+# --- model
+target_fn = lambda x: x**2
+pes = nengo.PES()
+
+with nengo.Network(label="ProfilePES", seed=3) as model:
+    x = nengo.Node(nengo.processes.WhiteSignal(runtime, high=1.), size_out=dims)
+    y = nengo.Node(size_in=dims)
+    nengo.Connection(x, y, function=target_fn)
+
+    a = nengo.Ensemble(n_neurons, dims)
+    b = nengo.Ensemble(n_neurons, dims)
+    nengo.Connection(x, a, synapse=None)
+
+    e = nengo.Ensemble(dims * neurons_per_dim, dims)
+    # nengo.Connection(b, e)  # time-consuming decoder finding involved
+    nengo.Connection(y, e, transform=-1)
+
+    c = nengo.Connection(a.neurons, b.neurons, transform=W0, learning_rule_type=pes)
+    nengo.Connection(e, c.learning_rule, synapse=None)
+
+# --- simulation
+sim = nengo_ocl.Simulator(model, context=ctx, profiling=True)
+sim.run(runtime)
+sim.print_profiling(sort=1)
