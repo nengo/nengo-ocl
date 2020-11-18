25908ce6031ff6cb5a8dd0a49c6e3627add46dff
Eric Hunsberger <eric.hunsberger@appliedbrainresearch.com>
2020-11-18 10:18:52 -0500

    Remove Signal.may_share_memory patch
    This is fixed in Nengo 3.1.0

--- nengo_ocl/__init__.py
+++ nengo_ocl/__init__.py
@@ -6,22 +6,6 @@ import logging
 import numpy as np
 import pyopencl as cl
 
-# --- monkey-patch Signal.may_share_memory
-# TODO: remove this monkeypatch once there is a fix in nengo core
-import nengo.builder.signal
-
-old_may_share_memory = nengo.builder.signal.Signal.may_share_memory
-
-
-def may_share_memory(self, other):
-    if self is other:
-        return True
-
-    return old_may_share_memory(self, other)
-
-
-nengo.builder.signal.Signal.may_share_memory = may_share_memory
-
 # --- imports from this package
 from .version import version as __version__
 from .simulator import Simulator
